0ab7f1dc85c4c7f6c2037848d640013a7df1bf6c
Affexter <Affexter@aol.com>
2024-09-12 12:12:55 -0400

    upd

--- pClasses/project1.py
+++ pClasses/project1.py
@@ -1,24 +1,13 @@
-class student():
-    def __init__(self):
-        self.name = ''
-        self.grade = []
-        
-        pgrades = {
-            0: range(0, 30),
-            1: range(30, 50),
-            2: range(50, 80),
-            3: range(80, 100)
-        }
-
-    def GetGrade(student):
-        pass
-
-                    
-
-student = student()
-
-print(student.settings)
-
+pgrades = {
+    0: range(0, 30),
+    1: range(30, 50),
+    2: range(50, 80),
+    3: range(80, 100)
+}
+class studentBody():
+    def Grades(self, name, grade):
+        self.name = name
+        self.grade = grade
+student = studentBody()
+print(student.name('jannice', 0))
 print(range(4, 8))
-
-##99, 77, 58, 87, 20
